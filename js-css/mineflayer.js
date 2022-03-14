df717e48e21bccfca0e8234afb1486206dadee2d
liamisawa <91570428+liamisawa@users.noreply.github.com>
2022-03-14 03:52:06 +0000

    Added content to mineflayer.js

--- js-css/mineflayer.js
+++ js-css/mineflayer.js
@@ -1,1 +1,7 @@
 
+const { token, username, password, host, version, help_username } = require('../json/config.json');
+const mineflayer = require('mineflayer');
+const bot = mineflayer.createBot({username:username,host:host,version:version}); bot.once('login', () => {console.log(`Bot has logged in` + host)});     bot.on('kicked', 'error', console.log);
+
+
+// Gonna see how well I can write a mineflayer bot on a single client
